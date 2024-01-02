8fe9479653aafeb5cf01225d2ef9993bae8eeebc
Mary Bruff <marybruff5@gmail.com>
2024-01-02 10:05:42 -0700

    create controlled form for reservations

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,68 @@
 import React from 'react'
 import './Form.css'
+import {useState} from 'react'
 
 const Form = () => {
+  const [name, setName] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted")
+  }
+
   return (
-    <div>Form</div>
+    <main>
+      <form className="reservation-form" onSubmit={handleSubmit}>
+        <input
+        type="text"
+        placeholder='Name'
+        value={name}
+        name='name'
+        className="input-field"
+        required
+        autoComplete='off'
+        onChange={e => setName(e.target.value)}
+        />
+        <input
+        type="text"
+        placeholder='Date'
+        value={date}
+        name='date'
+        className="input-field"
+        required
+        autoComplete='off'
+        onChange={e => setName(e.target.value)}
+        />
+        <input
+        type="text"
+        placeholder='Time'
+        value={time}
+        name='time'
+        className="input-field"
+        required
+        autoComplete='off'
+        onChange={e => setName(e.target.value)}
+        />
+        <input
+        type="number"
+        placeholder='Number of Guests'
+        value={number}
+        name='number'
+        className="input-field"
+        required
+        autoComplete='off'
+        onChange={e => setName(e.target.value)}
+        />
+        <button className='submit-button'>
+          Make Reservation
+        </button> 
+      </form>
+    </main>
+    
+
   )
 }
 
