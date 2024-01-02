e172d36eefa139c84d3355697be4245f2990089b
Mary Bruff <marybruff5@gmail.com>
2024-01-02 09:40:02 -0700

    useEffect for getReservations

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 import React from 'react';
+import getReservations from '../Api-calls';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [reservations, setReservations] = useState([]);
+
+  useEffect(() => {
+    getReservations()
+      .then(data => setReservations(data))
+      .catch(error => console.log(error))
+  } ,[])
+
   return (
     <div className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
