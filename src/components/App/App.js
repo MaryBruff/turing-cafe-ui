9d7b59c22cc65b53b96e4b396ad498b8f7fafa04
Mary Bruff <marybruff5@gmail.com>
2024-01-02 09:50:14 -0700

    display existing reservations no css yet

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React from 'react';
 import getReservations from '../Api-calls';
 import { useState, useEffect } from 'react';
+import Form from '../Form/Form';
+import Card from '../Card/Card';
 
 function App() {
 
@@ -13,12 +15,27 @@ function App() {
       .catch(error => console.log(error))
   } ,[])
 
+  const currentReservations = reservations.map(reservation => {
+    return (
+      <Card 
+        key={reservation.id} // key is required in map
+        id={reservation.id}
+        name={reservation.name}
+        date={reservation.date}
+        time={reservation.time}
+        number={reservation.number}
+      />
+    )
+  });
+
   return (
     <div className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
       <div className='resy-form'>
+        <Form />
       </div>
       <div className='resy-container'>
+        {currentReservations}
       </div>
     </div>
   );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Card.css'
 
-const Card = () => {
+const Card = ({id, name, date, time, number }) => {
   return (
-    <div>Card</div>
+    <section className="card">
+      <div className="card-content">
+        <h2 className="card-title">{name}</h2>
+        <div className="card-detail">
+          <p>{date}</p>
+          <p>{time}</p>
+          <p>Number of Guests: {number}</p>
+        </div>
+    <button className="button"> Cancel</button>
+      </div>
+    </section>
   )
 }
 
